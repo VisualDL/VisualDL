a4814b52b41c093896c150b9767b84420994b4fc
qiaolongfei <qiaolongfei@baidu.com>
2017-11-29 00:14:06 +0800

    init logdir runs tags and scalars interface

--- bin/visual_dl.py
+++ bin/visual_dl.py
@@ -4,6 +4,7 @@ import json
 import os
 import sys
 from optparse import OptionParser
+from flask import request
 
 from flask import Flask, redirect
 from flask import send_from_directory
@@ -27,27 +28,19 @@ def option_parser():
         action="store",
         dest="port",
         help="rest api service port")
+    parser.add_option(
+        "--logdir", action="store", dest="logdir", help="log file directory")
     return parser.parse_args()
 
 
-# return data
-# status, msg, data
-def gen_result(status, msg):
-    """
-
-    :param status:
-    :param msg:
-    :return:
-    """
-    result = dict()
-    result['status'] = status
-    result['msg'] = msg
-    result['data'] = {}
-    return result
-
-
+options, args = option_parser()
 server_path = os.path.abspath(os.path.dirname(sys.argv[0]))
 static_file_path = "../visualdl/frontend/dist/"
+
+
+@app.route("/")
+def index():
+    return redirect('/static/index.html', code=302)
 
 
 @app.route('/static/<path:filename>')
@@ -56,18 +49,28 @@ def serve_static(filename):
         os.path.join(server_path, static_file_path), filename)
 
 
-@app.route("/")
-def index():
-    return redirect('/static/index.html', code=302)
+@app.route('/data/logdir')
+def logdir():
+    return json.dumps({"logdir": options.logdir})
 
 
-@app.route('/hello')
-def hello():
-    result = gen_result(0, "Hello, this is VisualDL!")
-    return json.dumps(result)
+@app.route('/data/runs')
+def runs():
+    return ["train", "test"]
+
+
+@app.route("/data/plugin/scalars/tags")
+def tags():
+    return ["", ""]
+
+
+@app.route('/data/plugin/scalars/scalars')
+def scalars():
+    run = request.args.get('run')
+    tag = request.args.get('tag')
+    return None
 
 
 if __name__ == '__main__':
-    options, args = option_parser()
     logger.info(" port=" + str(options.port))
     app.run(debug=False, host="0.0.0.0", port=options.port)
